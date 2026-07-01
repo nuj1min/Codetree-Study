d03dfc27fbc2afd620d605d914fa9e5300487134
Kim MinJun <104950862+nuj1min@users.noreply.github.com>
2026-07-01 13:57:19 +0900

    [Passed] 비교 연산 (49ms, 16MB)

---
+++ trail1/비교 연산/comparison-operator.py
@@ -0,0 +1,41 @@
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+
+# a >= b
+if a >= b:
+    print(1)
+else:
+    print(0)
+
+# a > b
+if a > b:
+    print(1)
+else:
+    print(0)
+
+# a <= b
+if a <= b:
+    print(1)
+else:
+    print(0)
+
+# a < b
+if a < b:
+    print(1)
+else:
+    print(0)
+
+# a == b
+if a == b:
+    print(1)
+else:
+    print(0)
+
+# a != b
+if a != b:
+    print(1)
+else:
+    print(0)
