c6c423e980a765806b57906da896c29bdbdc6d32
Kim MinJun <104950862+nuj1min@users.noreply.github.com>
2026-07-01 14:30:35 +0900

    [Passed] 숫자의 증감 (46ms, 16MB)

---
+++ trail1/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,13 @@
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+c = arr[0]
+n = int(arr[1])
+
+# 출력
+if c == 'A': 
+    for i in range(1, n + 1):
+        print(i, end=" ")
+else:
+    for i in range(n, 0, -1):
+        print(i, end=" ")
