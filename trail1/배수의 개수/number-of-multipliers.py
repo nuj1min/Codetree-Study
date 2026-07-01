e6402f9b732dddf9e24dc1ceae75d5fa0cc89d61
Kim MinJun <104950862+nuj1min@users.noreply.github.com>
2026-07-01 14:37:06 +0900

    [Passed] 배수의 개수 (50ms, 16MB)

---
+++ trail1/배수의 개수/number-of-multipliers.py
@@ -0,0 +1,13 @@
+# 변수 선언, 입력
+cnt3, cnt5 = 0, 0
+
+for _ in range(10):
+    a = int(input())
+    
+    if a % 3 == 0:
+        cnt3 += 1
+    if a % 5 == 0:
+        cnt5 += 1
+
+# 출력
+print(cnt3, cnt5)
