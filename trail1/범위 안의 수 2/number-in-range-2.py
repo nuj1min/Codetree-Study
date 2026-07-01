f7b58191d9cd92f4b31abf396f01e11b7cd2c7dd
Kim MinJun <104950862+nuj1min@users.noreply.github.com>
2026-07-01 14:40:18 +0900

    [Passed] 범위 안의 수 2 (57ms, 16MB)

---
+++ trail1/범위 안의 수 2/number-in-range-2.py
@@ -0,0 +1,15 @@
+# 변수 선언, 입력
+sum_val = 0
+cnt = 0
+
+for _ in range(10):
+    num = int(input())
+    if num >= 0 and num <= 200:
+        sum_val += num
+        cnt += 1
+
+# 0이상 200이하의 정수들의 평균을 구합니다.
+avg = sum_val / cnt
+
+# 출력
+print(f"{sum_val} {avg:.1f}")
