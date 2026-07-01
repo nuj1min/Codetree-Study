8d6513dda53f0d2b8c27602bc94fc4d18eebe048
Kim MinJun <104950862+nuj1min@users.noreply.github.com>
2026-07-01 14:00:08 +0900

    [Passed] 특정 조건 세 정수 비교 (53ms, 16MB)

---
+++ trail1/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,17 @@
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+# 출력
+if a <= b and a <= c:
+    print("1", end=" ")
+else:
+    print("0", end=" ")
+
+if a == b and b == c:
+    print("1")
+else:
+    print("0")
