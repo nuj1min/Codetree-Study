8aca093bd1598ad3f9db778b8ba858bde2324135
Kim MinJun <104950862+nuj1min@users.noreply.github.com>
2026-07-01 14:35:18 +0900

    [Passed] 369 게임 (49ms, 16MB)

---
+++ trail1/369 게임/369-game.py
@@ -0,0 +1,13 @@
+# 변수 선언, 입력
+n = int(input())
+
+# 출력
+for i in range(1, n+1):
+    if i % 3 == 0:
+        print("0", end=" ")
+    elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+        print("0", end=" ")
+    elif i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
+        print("0", end=" ")
+    else:
+        print(i, end=" ")
