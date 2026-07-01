477bf35beb75d64a751e483f19750779dcbce4ac
Kim MinJun <104950862+nuj1min@users.noreply.github.com>
2026-07-01 13:55:00 +0900

    [Passed] 두 수의 짝홀 여부 (49ms, 16MB)

---
+++ trail1/두 수의 짝홀 여부/parity-of-two-numbers.py
@@ -0,0 +1,16 @@
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+
+# 출력
+if a % 2 == 0:
+    print("even")
+else:
+    print("odd")
+    
+if b % 2 == 0:
+    print("even")
+else:
+    print("odd")
