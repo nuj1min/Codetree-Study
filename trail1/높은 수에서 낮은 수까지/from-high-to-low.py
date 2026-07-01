4d8f332d60aad77bea9967d7f05ad20b6be8cf26
Kim MinJun <104950862+nuj1min@users.noreply.github.com>
2026-07-01 14:30:50 +0900

    [Passed] 높은 수에서 낮은 수까지 (47ms, 16MB)

---
+++ trail1/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,13 @@
+# 변수 선언, 입력
+inp = input()
+arr = inp.split()
+a = int(arr[0])
+b = int(arr[1])
+
+# 출력    
+if b >= a:
+    for i in range(b, a - 1, -1):
+        print(i, end=" ")
+else:
+    for i in range(a, b - 1, -1):
+        print(i, end=" ")
